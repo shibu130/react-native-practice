7121e1040a3275e21b8d8d4f15da54af8a52b4bc
shibuthomas-2022mt93003 <2022mt93003@wilp.bits-pilani.ac.in>
2024-10-02 18:23:41 +0530

    init

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>working on the first application</Text>
       <StatusBar style="auto" />
+      <Button title='clickme'></Button>
     </View>
   );
 }
